65540f00e1666beb6ce5417d9bc4a344aac18db0
Sherif Elmetwally <sge5@njit.edu>
2020-09-24 19:02:08 -0400

    idek

--- src/index.js
+++ src/index.js
@@ -23,6 +23,31 @@ app.post('/users', (req, res) => {
     })
 })
 
+app.get('/users', (req, res) => {
+    User.find({})
+    .then((users) => {
+        res.send(users)
+    })
+    .catch((e) => {
+        res.status(500).send()
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params._id
+    User.findById(_id)
+    .then((user) => {
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    })
+    .catch((e) => {
+        res.status(500).send()
+    })
+})
+
 app.post('/tasks', (req, res) => {
     const task = new Task(req.body)
 
