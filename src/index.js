ee25c7c39bbe7d7d128b07364b0240f6d8cdaae4
Sherif Elmetwally <sge5@njit.edu>
2020-09-25 00:13:32 -0400

    add endpoints for tasks

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ app.get('/users', (req, res) => {
 })
 
 app.get('/users/:id', (req, res) => {
-    const _id = req.params._id
+    const _id = req.params.id
     User.findById(_id)
     .then((user) => {
         if (!user) {
@@ -60,6 +60,31 @@ app.post('/tasks', (req, res) => {
         res.status(400)
         res.send(error)
         console.log(error)
+    })
+})
+
+app.get('/tasks', (req, res) => {
+    Task.find({})
+    .then((tasks) => {
+        res.send(tasks)
+    })
+    .catch((e) => {
+        res.status(500).send()
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id
+    Task.findById(_id)
+    .then((task) => {
+        if (!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    })
+    .catch((e) => {
+        res.status(500).send()
     })
 })
 
